207c9a8005727db2faec81886cb2c26ebe55aa5a
lopesjorge <jorgelopeslopes@outlook.com>
2025-01-22 16:59:31 -0300

    url teste

--- projeto1/urls.py
+++ projeto1/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+#HTTP REQUEST
+def my_view (request):
+    return HttpResponse('Olá mundo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
