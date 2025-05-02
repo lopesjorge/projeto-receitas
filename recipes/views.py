a8b301d453b6abf9f0529147d1c1ea69dbc1c951
lopesjorge <jorgelopeslopes@outlook.com>
2025-05-02 17:59:03 -0300

    feat:separando tests por views

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.http.response import Http404
 from recipes.models import Recipe
 
 def home (request):
@@ -38,4 +39,12 @@ def recipe (request, recipe_id):
 
 
 def search (request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q','').strip()
+ 
+    if not search_term:
+        raise Http404()
+    
+    return render(request, 'recipes/pages/search.html',context={
+        'page_title':f'Search for: "{search_term}"',
+        'search_term':search_term,
+    })
